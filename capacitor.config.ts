a3b509be7bc50ea37e6bbd0a4de8a148429722dd
BohdanKovalets <kovaletss.bogdan@gmail.com>
2025-05-16 14:36:12 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cleaning-services',
+  webDir: 'www'
+};
+
+export default config;
